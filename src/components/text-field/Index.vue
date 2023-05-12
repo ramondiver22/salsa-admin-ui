5b02ad4fb9398d8eb855b73e7189e93c21116cc5
Alexandre Castro <alexandrecotrimcastro@gmail.com>
2023-05-12 08:52:39 -0300

    feat: custom text field component

---
+++ src/components/text-field/Index.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-text-field variant="solo" density="comfortable" :label="label" v-model="model"></v-text-field>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  props: {
+    label: {
+      type: String,
+      required: true
+    },
+    value: {
+      type: String
+    }
+  },
+
+  computed: {
+    model: {
+      get() {
+        return this.value
+      },
+      set(value: String) {
+        this.$emit('update:modelValue', value)
+      }
+    }
+  }
+})
+</script>
